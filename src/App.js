2cd9d70d652b9402289c31cb79ddbeadec68d38d
Tony <tdoyler06@hotmail.com>
2023-02-02 06:21:30 +0000

    Palette Routes Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import Palette from "./Palette";
+import { Route, Switch } from "react-router-dom";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 function App() {
+	const findPalette = (id) => {
+		return seedColors.find(function (palette) {
+			return palette.id === id;
+		});
+	};
 	return (
-		<div>
-			<Palette palette={generatePalette(seedColors[4])} />
-		</div>
+		<Switch>
+			<Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
+			<Route
+				exact
+				path="/palette/:id"
+				render={(routeProps) => (
+					<Palette
+						palette={generatePalette(findPalette(routeProps.match.params.id))}
+					/>
+				)}
+			/>
+		</Switch>
+
+		// <div>
+		// 	<Palette palette={generatePalette(seedColors[4])} />
+		// </div>
 	);
 }
 
