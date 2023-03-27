b9ece7efd28ce3b34c5c786e21d8832ac7b10d63
Tony <tdoyler06@hotmail.com>
2023-03-27 06:31:20 +0100

    Palette list made responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from "./sizes";
+
 const styles = {
 	root: {
 		backgroundColor: "blue",
@@ -12,6 +14,12 @@ const styles = {
 		alignItems: "flex-start",
 		flexDirection: "column",
 		flexWrap: "wrap",
+		[sizes.down("xs")]: {
+			width: "70%",
+		},
+		[sizes.down("xl")]: {
+			width: "80%",
+		},
 	},
 	nav: {
 		display: "flex",
@@ -28,7 +36,14 @@ const styles = {
 		width: "100%",
 		display: "grid",
 		gridTemplateColumns: "repeat(3, 30%)",
-		gridGap: "5%",
+		gridGap: "1.5rem",
+		[sizes.down("sm")]: {
+			gridTemplateColumns: "repeat(2, 50%)",
+		},
+		[sizes.down("xs")]: {
+			gridTemplateColumns: "repeat(1, 100%)",
+			gridGap: "1rem",
+		},
 	},
 };
 
