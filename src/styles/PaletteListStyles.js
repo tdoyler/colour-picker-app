694b45368a70056458d5fd67178c9fb69643ffd7
Tony <tdoyler06@hotmail.com>
2023-03-27 07:04:27 +0100

    Add SVG to PalettesList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,12 +1,21 @@
 import sizes from "./sizes";
+import bg from "./bg.svg";
 
 const styles = {
 	root: {
-		backgroundColor: "blue",
 		height: "100vh",
 		display: "flex",
 		alignItems: "flex-start",
 		justifyContent: "center",
+		// background by SVGBackgrounds.com
+		backgroundColor: "#A10FE4",
+		backgroundImage: `url(${bg})`,
+		backgroundAttachment: "fixed",
+		backgroundSize: "cover",
+		overflow: "scroll",
+	},
+	heading: {
+		fontSize: "2rem",
 	},
 	container: {
 		width: "50%",
