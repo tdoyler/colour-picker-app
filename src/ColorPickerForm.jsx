70703cb48aaf98546772b7153daf0f3fc7db8409
Tony <tdoyler06@hotmail.com>
2023-04-11 06:26:37 +0100

    Fix Issues With NewPaletteForm

--- src/ColorPickerForm.jsx
+++ src/ColorPickerForm.jsx
@@ -51,7 +51,7 @@ class ColorPickerForm extends Component {
 					onChangeComplete={this.updateCurrentColor}
 					className={classes.picker}
 				/>
-				<ValidatorForm onSubmit={this.handleSubmit}>
+				<ValidatorForm onSubmit={this.handleSubmit} instantValidate={false}>
 					<TextValidator
 						placeholder="Color Name"
 						value={newColorName}
--- src/NewPaletteForm.jsx
+++ src/NewPaletteForm.jsx
@@ -12,6 +12,7 @@ import DraggableColorList from "./DraggableColorList";
 import { arrayMoveImmutable } from "array-move";
 import ColorPickerForm from "./ColorPickerForm";
 import styles from "./styles/NewPaletteFormStyles";
+import seedColors from "./seedColors";
 
 class NewPaletteForm extends Component {
 	static defaultProps = {
@@ -22,7 +23,7 @@ class NewPaletteForm extends Component {
 		this.state = {
 			open: true,
 
-			colors: this.props.palettes[0].colors,
+			colors: seedColors[0].colors,
 		};
 
 		this.addNewColor = this.addNewColor.bind(this);
